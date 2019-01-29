2b6740096773c08438c023f3aa7eb282c6e371bb
indra1997 <jagdaleindrajit@gmail.com>
2019-01-29 02:58:48 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+let model;
+
+const modelURL = 'http://localhost:5000/model';
+
+const preview = document.getElementById("preview");
+const predictButton = document.getElementById("predict");
+const clearButton = document.getElementById("clear");
+const numberOfFiles = document.getElementById("number-of-files");
+const fileInput = document.getElementById('file');
+
+const predict = async (modelURL) => {}
